5718752d35567f95798098b91f2c233189a33665
Jacob Roy <lejacobroy@gmail.com>
2019-04-24 21:52:20 -0400

    Updated stats

---
+++ src/_data/todayValues.js
@@ -0,0 +1,364 @@
+{
+    "mediumTermTodayValueBtc": {
+        "number": 0.12865385664244,
+        "percentage": "1.3"
+    },
+    "mediumTermTodayValueUsdt": {
+        "number": 698.4643607889396,
+        "percentage": "-0.2"
+    },
+    "mediumTermTodayQtyOrders": {
+        "number": 0,
+        "percentage": 0
+    },
+    "mediumTermTodaySellingTimeAvg": {
+        "number": "0.0 m",
+        "percentage": 0
+    },
+    "mediumTermTodayGainsBtc": {
+        "number": "0.0016"
+    },
+    "mediumTermTodayGainsUsdt": {
+        "number": "-1.2"
+    },
+    "shortTermTodayValueBtc": {
+        "number": 0.071179574475812,
+        "percentage": "3.3"
+    },
+    "shortTermTodayValueUsdt": {
+        "number": 386.43533342067286,
+        "percentage": "1.9"
+    },
+    "shortTermTodayQtyOrders": {
+        "number": 0,
+        "percentage": 0
+    },
+    "shortTermTodaySellingTimeAvg": {
+        "number": "0.0 m",
+        "percentage": 0
+    },
+    "shortTermTodayGainsBtc": {
+        "number": "0.0024"
+    },
+    "shortTermTodayGainsUsdt": {
+        "number": "7.2"
+    },
+    "longTermTodayValueBtc": {
+        "number": 0.33458553489614395,
+        "percentage": "1.0"
+    },
+    "longTermTodayValueUsdt": {
+        "number": 1816.4849440832595,
+        "percentage": "-0.4"
+    },
+    "longTermTodayQtyOrders": {
+        "number": 0,
+        "percentage": 0
+    },
+    "longTermTodaySellingTimeAvg": {
+        "number": "0.0 m",
+        "percentage": 0
+    },
+    "longTermTodayGainsBtc": {
+        "number": "0.0035"
+    },
+    "longTermTodayGainsUsdt": {
+        "number": "-8.1"
+    },
+    "longerTermTodayValueBtc": {
+        "number": 0.17459705786619004,
+        "percentage": "1.0"
+    },
+    "longerTermTodayValueUsdt": {
+        "number": 947.8979029790178,
+        "percentage": "-0.4"
+    },
+    "longerTermTodayQtyOrders": {
+        "number": 0,
+        "percentage": 0
+    },
+    "longerTermTodaySellingTimeAvg": {
+        "number": "0.0 m",
+        "percentage": 0
+    },
+    "longerTermTodayGainsBtc": {
+        "number": "0.0018"
+    },
+    "longerTermTodayGainsUsdt": {
+        "number": "-4.2"
+    },
+    "portfolioTodayValueBtc": {
+        "number": 0.709016023880586,
+        "percentage": "1.3"
+    },
+    "portfolioTodayValueUsdt": {
+        "number": 3849.2825412718903,
+        "percentage": "-0.2"
+    },
+    "portfolioTodayQtyOrders": {
+        "number": 0,
+        "percentage": 0
+    },
+    "portfolioToday": {
+        "name": "Portfolio Value",
+        "display": true,
+        "type": "area-spline",
+        "hide-legend": true,
+        "categories": [
+            15,
+            16,
+            16,
+            17,
+            18,
+            19,
+            19,
+            19,
+            20,
+            21,
+            21,
+            22
+        ],
+        "series": [
+            {
+                "name": "BTC",
+                "color": "yellow",
+                "data": [
+                    0.7528273621638699,
+                    0.759085485980112,
+                    0.7601467767429702,
+                    0.783637696414713,
+                    0.779356794024025,
+                    0.78854331209223,
+                    0.784943924581298,
+                    0.779918316848714,
+                    0.764032126206458,
+                    0.7567406629215019,
+                    0.725113601989134,
+                    0.746281758911976
+                ]
+            }
+        ]
+    },
+    "shortTermToday": {
+        "name": "Short Value",
+        "display": true,
+        "type": "area-spline",
+        "hide-legend": true,
+        "categories": [
+            12,
+            13,
+            14,
+            15,
+            16,
+            18,
+            19,
+            20,
+            21,
+            22,
+            23,
+            24
+        ],
+        "series": [
+            {
+                "name": "BTC",
+                "color": "yellow",
+                "data": [
+                    0.071202801571232,
+                    0.071214737045244,
+                    0.069899872779342,
+                    0.07445274480320901,
+                    0.074884118789096,
+                    0.07526011403101998,
+                    0.07567693204181199,
+                    0.074586738176464,
+                    0.072912335875576,
+                    0.07434201043052,
+                    0.068826195896864,
+                    0.071179574475812
+                ]
+            }
+        ]
+    },
+    "mediumTermToday": {
+        "name": "Medium Value",
+        "display": true,
+        "type": "area-spline",
+        "hide-legend": true,
+        "categories": [
+            15,
+            16,
+            16,
+            17,
+            18,
+            19,
+            19,
+            19,
+            20,
+            21,
+            21,
+            22,
+            22,
+            23,
+            24
+        ],
+        "series": [
+            {
+                "name": "BTC",
+                "color": "yellow",
+                "data": [
+                    0.14329651560675602,
+                    0.14156093764896197,
+                    0.141561129648962,
+                    0.14056354745209998,
+                    0.1379535314514,
+                    0.13653529513370402,
+                    0.13653529513370402,
+                    0.13643427348775203,
+                    0.136029667979922,
+                    0.13581982285237698,
+                    0.1358497824488,
+                    0.137621959995594,
+                    0.13758362740065,
+                    0.127020445752845,
+                    0.12865385664244
+                ]
+            }
+        ]
+    },
+    "longTermToday": {
+        "name": "Long Value",
+        "display": true,
+        "type": "area-spline",
+        "hide-legend": true,
+        "categories": [
+            12,
+            13,
+            14,
+            15,
+            15,
+            16,
+            16,
+            18,
+            19,
+            20,
+            21,
+            22,
+            23,
+            24
+        ],
+        "series": [
+            {
+                "name": "BTC",
+                "color": "yellow",
+                "data": [
+                    0.351988344637599,
+                    0.35415393369912807,
+                    0.35599098154887604,
+                    0.37008297826740305,
+                    0.37008297826740305,
+                    0.377932663693306,
+                    0.37794272409838203,
+                    0.382326566863,
+                    0.3735922896664,
+                    0.35747821652907197,
+                    0.34766333648416,
+                    0.36288316657437997,
+                    0.33113422170693596,
+                    0.33458553489614395
+                ]
+            }
+        ]
+    },
+    "longerTermToday": {
+        "name": "Longer Value",
+        "display": true,
+        "type": "area-spline",
+        "hide-legend": true,
+        "categories": [
+            12,
+            13,
+            14,
+            15,
+            16,
+            18,
+            19,
+            20,
+            21,
+            22,
+            23,
+            24
+        ],
+        "series": [
+            {
+                "name": "BTC",
+                "color": "yellow",
+                "data": [
+                    0.186339700348283,
+                    0.192155877586778,
+                    0.19269479276579,
+                    0.198538425892001,
+                    0.196436165516126,
+                    0.19881523923419997,
+                    0.1947889733074,
+                    0.186570738321498,
+                    0.18149783268456,
+                    0.189100613109533,
+                    0.17277428715930998,
+                    0.17459705786619004
+                ]
+            }
+        ]
+    },
+    "barStackedToday": {
+        "name": "Accounts Value in Portfolio",
+        "display": true,
+        "type": "bar",
+        "hide-legend": false,
+        "categories": [
+            "Short Term",
+            "Medium Term",
+            "Long Term",
+            "Longer Term"
+        ],
+        "series": [
+            {
+                "name": "Account Value in BTC",
+                "color": "blue",
+                "data": [
+                    0.071179574475812,
+                    0.12865385664244,
+                    0.33458553489614395,
+                    0.17459705786619004
+                ]
+            }
+        ]
+    },
+    "donutToday": {
+        "name": "Accounts % in Portfolio",
+        "display": true,
+        "type": "donut",
+        "hide-legend": false,
+        "series": [
+            {
+                "name": "Short Term",
+                "color": "blue-light",
+                "data": 10.039205332233818
+            },
+            {
+                "name": "Medium Term",
+                "color": "green-light",
+                "data": 18.145408891930508
+            },
+            {
+                "name": "Long Term",
+                "color": "yellow-light",
+                "data": 47.19012316038933
+            },
+            {
+                "name": "Long Term",
+                "color": "red-light",
+                "data": 24.62526261544634
+            }
+        ]
+    }
+}
